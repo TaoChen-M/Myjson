20e1563e86340cb165ba4abf76e558f6d77b1365
陈涛 <chentao_email_m@163.com>
2022-12-03 17:42:58 +0800

    Add files via upload

---
+++ tutorial02/leptjson.c
@@ -0,0 +1,141 @@
+#include "leptjson.h"
+#include <assert.h>  /* assert() */
+#include <stdlib.h>  /* NULL, strtod() */
+#include <math.h>    /* HUGE_VAL */
+
+#define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+
+/*宏定义辅助函数*/
+#define ISDIGIT(ch)       ((ch)>='0'&&(ch)<='9')
+#define ISDIGIT1TO9(ch)       ((ch)>='1'&&(ch)<='9')
+
+typedef struct {
+    const char *json;
+} lept_context;
+
+static void lept_parse_whitespace(lept_context *c) {
+    const char *p = c->json;
+    while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
+        p++;
+    c->json = p;
+}
+
+/*
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_null(lept_context* c, lept_value* v) {
+    EXPECT(c, 'n');
+    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_NULL;
+    return LEPT_PARSE_OK;
+}
+ */
+static int lept_parse_literal(lept_context *c, lept_value *v, char *cmp, size_t n, int typevalue) {
+    EXPECT(c, cmp[0]);
+    cmp++;
+    size_t i;
+    for (i = 1; i < n; ++i) {
+        if (*cmp != *c->json)return LEPT_PARSE_INVALID_VALUE;
+        cmp++;
+        c->json++;
+    }
+    v->type = typevalue;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_number(lept_context *c, lept_value *v) {
+    /*使用一个新的指针来辅助判断数字是否是合法得*/
+    const char *p = c->json;
+    /* \TODO validate number */
+    /*先判断如果含有负号，如果有指针直接前移*/
+    if (*p == '-') {
+        p++;
+    }
+    /*首字母是否是0*/
+    if (*p == '0')p++;
+        /*如果首字不是0，则必须是1-9之间得数字*/
+    else {
+        if (!ISDIGIT1TO9(*p))return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    /*如果出现小数点，小数点之后至少要有一个数字，不是数字就返回错误，之后跳过所有得数字*/
+    if (*p == '.') {
+        p++;
+        if (!ISDIGIT(*p))return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    /*如果出现e、E，之后至少要有一个数字，不是数字就返回错误，之后跳过所有得数字*/
+    if (*p == 'e' || *p == 'E') {
+        p++;
+        if (*p == '+' || *p == '-')p++;
+        if (!ISDIGIT(*p))return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    errno = 0;
+    v->n = strtod(c->json, NULL);
+    if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+        return LEPT_PARSE_NUMBER_TOO_BIG;
+    c->json = p;
+    v->type = LEPT_NUMBER;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_value(lept_context *c, lept_value *v) {
+    switch (*c->json) {
+        case 't':
+            return lept_parse_literal(c, v, "true", 4, LEPT_TRUE);
+        case 'f':
+            return lept_parse_literal(c, v, "false", 5, LEPT_FALSE);
+        case 'n':
+            return lept_parse_literal(c, v, "null", 4, LEPT_NULL);
+        default:
+            return lept_parse_number(c, v);
+        case '\0':
+            return LEPT_PARSE_EXPECT_VALUE;
+    }
+}
+
+int lept_parse(lept_value *v, const char *json) {
+    lept_context c;
+    int ret;
+    assert(v != NULL);
+    c.json = json;
+    v->type = LEPT_NULL;
+    lept_parse_whitespace(&c);
+    if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0') {
+            v->type = LEPT_NULL;
+            ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+        }
+    }
+    return ret;
+}
+
+lept_type lept_get_type(const lept_value *v) {
+    assert(v != NULL);
+    return v->type;
+}
+
+double lept_get_number(const lept_value *v) {
+    assert(v != NULL && v->type == LEPT_NUMBER);
+    return v->n;
+}
